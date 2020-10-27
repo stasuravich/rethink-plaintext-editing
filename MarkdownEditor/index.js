a097c6c6ecd1e517056feb06bf1910a46643a900
stasuravich <stasuravich@gmail.com>
2020-10-26 23:43:55 -0700

    Update index.js

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import css from './style.css';
+import ReactMarkdown from 'react-markdown';
+import {render} from 'react-dom';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 function MarkdownEditor({ file, write }) {
+  const [data, setData] = useState();
   console.log(file, write);
+  const reader = new FileReader();
+  reader.readAsText(file)
+  reader.onload = function() {
+    const text = reader.result;
+    setData(text);
+  };
+  console.log(file, write);
+  const [input = data, setInput] = useState();
   return (
-    <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/markdown</i>
+    <div>
+      <h3>Markdown Editor</h3>
+      <div className={css.editor}>
+
+        <textarea defaultValue ={data} autoFocus className="textarea" value={input} onChange={
+          (e) => setInput(e.target.value)}/>
+
+        <ReactMarkdown source={input} className="markdown"
+        renderers={{code: Component,}}
+        />
+
+      </div>
     </div>
+
   );
 }
+
+
+const Component = ({value, language}) => {
+  return (
+    <SyntaxHighlighter language={language ?? null} style={docco}>
+      {value ?? ''}
+    </SyntaxHighlighter>
+  );
+};
 
 MarkdownEditor.propTypes = {
   file: PropTypes.object,
