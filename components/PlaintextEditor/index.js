746a1038d1720229d81a9a6b0a3b3de7da2e2db8
stasuravich <stasuravich@gmail.com>
2020-10-26 23:44:54 -0700

    Update index.js

--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import PropTypes from 'prop-types';
-
 import css from './style.css';
 
+
+
 function PlaintextEditor({ file, write }) {
+  const [data, setData] = useState();
   console.log(file, write);
-  return (
-    <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
-    </div>
-  );
+  const reader = new FileReader();
+  reader.readAsText(file)
+  reader.onload = function() {
+    const text = reader.result;
+    setData(text);
+  };
+  if (typeof window !== 'undefined') {
+    const CKEditor = require('@ckeditor/ckeditor5-react');
+    const ClassicEditor = require('@ckeditor/ckeditor5-build-classic');
+    return (
+      <div className={css.editor}>
+        <h3>Text Editor</h3>
+        <CKEditor
+          editor={ClassicEditor}
+          data = {data}
+        />
+      </div>
+    );
+  }
 }
 
 PlaintextEditor.propTypes = {
